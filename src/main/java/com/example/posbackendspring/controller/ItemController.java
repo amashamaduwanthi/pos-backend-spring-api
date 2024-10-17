0ec7625578ee51a12307dd184807dc6e0142ff46
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2024-10-17 14:05:59 +0530

    add logging and regex for item update delete

--- src/main/java/com/example/posbackendspring/controller/ItemController.java
+++ src/main/java/com/example/posbackendspring/controller/ItemController.java
@@ -3,7 +3,9 @@ package com.example.posbackendspring.controller;
 import com.example.posbackendspring.dto.impl.CustomerDTO;
 import com.example.posbackendspring.dto.impl.ItemDTO;
 import com.example.posbackendspring.exception.DataPersistException;
+import com.example.posbackendspring.exception.ItemNotFoundException;
 import com.example.posbackendspring.service.ItemService;
+import com.example.posbackendspring.util.RegEx;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,16 +41,41 @@ public class ItemController {
     @PutMapping(value = "/{itemCode}")
     public ResponseEntity<Void> updateCustomer(@PathVariable("itemCode") String itemCode,
                                                @RequestBody ItemDTO updateditemDto) {
+        try {
+            if(!RegEx.itemCodeMatcher(itemCode) || updateditemDto == null){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
         //validation
         itemService.updateItem(itemCode, updateditemDto);
+            logger.info("Item update successful");
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        }catch (ItemNotFoundException e){
+            logger.warn("Returning 400 Bad Request",e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            logger.error("Item update unsuccessful",e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 
     @DeleteMapping(value = "/{itemCode}")
     public ResponseEntity<Void> deleteCustomer(@PathVariable("itemCode") String itemCode) {
+        try {
+            if(!RegEx.itemCodeMatcher(itemCode)){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
         itemService.deleteItem(itemCode);
+            logger.info("Item delete successful");
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (ItemNotFoundException e){
+            logger.warn("Returning 404 Not Found",e.getMessage());
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch (Exception e){
+            logger.error("Item delete unsuccessful",e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
